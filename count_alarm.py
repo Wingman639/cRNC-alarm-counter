037bdeb3a060a417520316d99f63abd924139cab
Yan Junhui <wingman639@gmail.com>
2014-12-22 11:32:43 +0800

    baseline. works. Use string.find() to get alarm number and count

---
+++ count_alarm.py
@@ -0,0 +1,65 @@
+# -*- coding: utf-8 -*-
+
+
+SOURCE_FILE_NAME = 'alarm_log.txt'
+REPORT_FILE_NAME = 'count_report.txt'
+
+
+def alarmTextListWithText(text):
+	return text.split(') IPA2800          ')
+
+def alarmNumFromText(text):
+	iAlarm = text.find(' ALARM ')
+	if iAlarm < 1: 
+		return None
+	alarmStr = text[iAlarm: iAlarm + 130]
+	iAlarmNumPrefix = alarmStr.find('\n    ')
+	alarmNumStr = alarmStr[iAlarmNumPrefix+5: iAlarmNumPrefix + 10]
+	return alarmNumStr
+
+def collectAlarms(alarmTextList):
+	alarms = {}
+	for alarmText in alarmTextList:
+		alarmNum = alarmNumFromText(alarmText)
+		if alarmNum is None:
+			continue
+		
+		if alarmNum in alarms:
+			alarms[alarmNum] += 1
+		else:
+			alarms[alarmNum] = 1
+	return alarms
+
+
+def readTextFile(fileName):
+	f = open(fileName, 'r')
+	text = f.read()
+	f.close()
+	return text
+
+def writeReport(text):
+	f = open(REPORT_FILE_NAME, 'w')
+	f.write(text)
+	f.close()
+
+def dictToStr(dict):
+	text = ''
+	for key, value in dict.iteritems():
+		text += '\n' + str(key) + ' : ' + str(value)
+	return text
+
+
+def counting(text):
+	alarmTextList = alarmTextListWithText(text)
+	alarms = collectAlarms(alarmTextList)
+	print alarms
+	report = dictToStr(alarms)
+	return report
+
+def main():
+	text = readTextFile(SOURCE_FILE_NAME)
+	report = counting(text)
+	writeReport(report)
+
+if __name__ == '__main__':
+	main()
